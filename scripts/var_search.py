cbaf588ce1a3386ed6d0d5990530e4d5ae28c72e
NGnius <ngniusness@gmail.com>
2018-05-08 10:49:38 -0400

    Fix directory issues

--- scripts/var_search.py
+++ scripts/var_search.py
@@ -5,6 +5,7 @@ import json_searcher
 
 PATH_INDICATOR = ">>"
 ASSOCIATION_INDICATOR = ":"
+DEFAULT_DIRECTORY = "C:\\Games\\CardLife"
 
 def get_input(prompt, options={"y", "n", ""}):
     '''covenience function for nagging users for an input'''
@@ -21,16 +22,23 @@ def process_results(results_in):
             results_out += "\t"+str(result[-2])+ASSOCIATION_INDICATOR+str(result[-1])+" in "
             for i in range(len(result)-2):
                 if i == len(result)-3:
-                    results_out+=result[i]+"\n"
+                    results_out+=str(result[i])
                 else:
-                    results_out+=result[i]+PATH_INDICATOR
+                    results_out+=str(result[i])+PATH_INDICATOR
+            results_out+="\n"
+    if results_out == "":
+        results_out="No results"
     return results_out
 another = True
 while another:
     query = input("What term would you like to search for? ")
-    directory = '/home/ngnius/Documents/CardLife' #input("Where would you like to look for it? ")
+    directory = input("Where would you like to look for it? ")
+    if directory == "":
+        directory = DEFAULT_DIRECTORY
+    print("Searching...")
     results = json_searcher.find(directory, query)
+    print("Results")
     print(process_results(results))
-    response = get_input("Refine query? [Y/n]")
+    response = get_input("Another query? [Y/n]")
     if response == "n":
         another = False
